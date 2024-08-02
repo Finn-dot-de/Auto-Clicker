596c44ea8b8b6738d30296875a56b54facb94564
Finn <finnlaskowski761@gmail.com>
2024-08-02 08:54:30 +0200

    Für linux und windows ausführbare Datei Scripte hinzugefügt

--- build.ps1
+++ build.ps1
@@ -1,5 +1,3 @@
 pip install -r requirement.txt
 
-pyinstaller --onefile --noconsole --add-data "src;src" --icon=icon.ico main.py
-
-pyinstaller --onefile --noconsole --add-data "src:src" --icon=icon.ico main.py
+pyinstaller --onefile --noconsole --add-data "src;src" --icon=icon.ico main.py # Windows
---
+++ build.sh
@@ -0,0 +1,3 @@
+pip install -r requirement.txt
+
+pyinstaller --onefile --noconsole --add-data "src:src" --icon=icon.ico main.py # Linux
